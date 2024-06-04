a43419279617df3feee5a3e0e608cf715e71070b
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-03 22:26:17 -0500

    aaa

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,25 @@
+import Head from "next/head";
+import Link from "next/link";
+import {Nav} from '/src/components/nav.js'
+
+
 import { useEffect, useState } from "react";
 
 export default function Events() {
 
   return (
     <div>
-        
+        <Head>
+        <title>Bopl Battle Competitive</title>
+        </Head>
+        <header>
+            <Nav />
+        </header>
+        <main className="container">
+            <section className="page-section">
+
+            </section>
+        </main>
     </div>
   );
 }
