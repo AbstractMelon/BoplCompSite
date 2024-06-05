b37942a57802387b1acdfcf2a0766af8e3e5e593
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-04 20:29:07 -0500

    aaa

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,10 @@
 import React from 'react';
+
 import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
 
 import  {getAdminHashes} from '/utils/hash.js'
 import { parseCookies } from '/utils/cookies';
@@ -8,7 +13,14 @@ import { parseCookies } from '/utils/cookies';
 const ProtectedPage = () => {
   return (
     <div className='dashboard'>
-      
+        <div className='makeContainer' style={{height:"400px",width:"100%"}}>
+          <h1 style={{color:"#fff"}}>Create New Announcement/Event</h1>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <br></br>
+          <Button variant="contained" endIcon={<SendIcon />} sx={{backgroundColor:"#2f2f31",margin:"10px"}}>Publish Announcement</Button>
+          <Button variant="contained" endIcon={<SendIcon />} sx={{backgroundColor:"#2f2f31",margin:"10px"}}>Publish Event</Button>
+        </div>
+
     </div>
   );
 };
